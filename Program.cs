c337276f7a05e82b3fba9c0fa3087f3586bbc672
LuisRoca11 <lroca1102@gmail.com>
2025-09-09 16:07:07 -0300

    Se ingresa comparador if

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Se ingresan por teclado tres números, si al menos uno de los valores ingresados es menor a 10, 
+// imprimir en pantalla la leyenda "Alguno de los números es menor a diez".
+
+using System;
+using System.Collections.Specialized;
+
+
+namespace tres_num_dif
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Incresar numero 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Incresar numero 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Incresar numero 3: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1<10 || num2<10 || num3<10)
+            {
+                Console.WriteLine("Alguno de los números es menor a diez");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
